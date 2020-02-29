980327ad272276e04730e20073fb6f9596e309c5
Din Lanh <langdinhieu@gmail.com>
2020-02-29 14:01:53 -0500

    protected posts routes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth');
+    }
     //
     public function create() {
         return view('posts.create');
