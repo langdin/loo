368b0d2b261733d8c91f51eb2959c08e84d232c0
Din Lanh <langdinhieu@gmail.com>
2020-02-29 23:03:27 -0500

    fixed: edit profile image update

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -30,18 +30,20 @@ class ProfilesController extends Controller
             'image' => 'image',
         ]);
 
+
         if (request('image')) {
             $imagePath = request('image')->store('profile', 'public');
 
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
             $image->save();
 
+            $imageArr = ['image' => $imagePath];
         }
 
 
         auth()->user()->profile->update(array_merge(
             $data,
-            ['image' => $imagePath]
+            $imageArr ?? []
         ));
 
         return redirect('/profile/' . $user->id);
