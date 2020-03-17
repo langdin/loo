f76b71bd3acab03175544806b2e18734aa4e1dc7
Din Lanh <langdinhieu@gmail.com>
2020-03-17 16:53:06 -0400

    created message model

---
+++ app/Message.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace loo;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    //
+}
--- app/User.php
+++ app/User.php
@@ -58,4 +58,8 @@ class User extends Authenticatable
     public function following() {
         return $this->belongsToMany(Profile::class);
     }
+
+    public function messages() {
+        return $this->hasMany(Message::class);
+    }
 }
