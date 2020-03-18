656c0efdc56719f3c0dfed1d6fffdb5320cef66e
Din Lanh <langdinhieu@gmail.com>
2020-03-18 12:22:33 -0400

    finished user message relationship

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    /**
+    * Fields that are mass assignable
+    *
+    * @var array
+    */
+    protected $fillable = ['message'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
